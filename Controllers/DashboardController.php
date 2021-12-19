e187fc6b09f615881afd2924a5d16ba06b28d7cb
DiegoAGtz <diegogtz1207@gmail.com>
2021-12-18 22:37:23 -0600

    Correcciones de autentificación

--- Controllers/DashboardController.php
+++ Controllers/DashboardController.php
@@ -5,58 +5,82 @@ require_once 'Models/SaleModel.php';
 
 class DashboardController extends BaseController {
     public function index() {
-        $sale = new SaleModel();
-        $purchases = $sale->where('user_id', Auth::info()['id']);
-        foreach ($purchases as $purchase) {
-            $totalProducts[] = $sale->total($purchase['id']);
+        if(Auth::loggedIn()) {
+            $sale = new SaleModel();
+            $purchases = $sale->where('user_id', Auth::info()['id']);
+            foreach ($purchases as $purchase) {
+                $totalProducts[] = $sale->total($purchase['id']);
+            }
+            foreach ($totalProducts as $key => $total) {
+                $shopping[] = array(
+                    'id' => $purchases[$key]['id'],
+                    'date' => $purchases[$key]['date'],
+                    'totalProducts' => $totalProducts[$key][0]['total'],
+                    'totalCost' => $totalProducts[$key][0]['cost']
+                );
+            }
+            $this->view('dashboard/index', array(
+                'shopping' => $shopping,
+                'totalProducts' => $totalProducts
+            ));
+        } else {
+            $this->redirect('Auth', 'login');
         }
-        foreach ($totalProducts as $key => $total) {
-            $shopping[] = array(
-                'id' => $purchases[$key]['id'],
-                'date' => $purchases[$key]['date'],
-                'totalProducts' => $totalProducts[$key][0]['total'],
-                'totalCost' => $totalProducts[$key][0]['cost']
-            );
-        }
-        $this->view('dashboard/index', array(
-            'shopping' => $shopping,
-            'totalProducts' => $totalProducts
-        ));
     }
 
     public function profile() {
-        $this->view('dashboard/profile');
+        if(Auth::loggedIn()) {
+            $this->view('dashboard/profile');
+        } else {
+            $this->redirect('Auth', 'login');
+        }
     }
 
     public function products() {
-        $product = new ProductModel();
-        $products = $product->where('user_id', Auth::info()['id']);
-        $this->view('dashboard/products', array(
-            'products' => $products
-        ));
+        if(Auth::loggedIn()) {
+            $product = new ProductModel();
+            $products = $product->where('user_id', Auth::info()['id']);
+            $this->view('dashboard/products', array(
+                'products' => $products
+            ));
+        } else {
+            $this->redirect('Auth', 'login');
+        }
     }
 
     public function purchase() {
-        $sale = new SaleModel();
-        if(isset($_GET['id'])) {
-            $products = $sale->products($_GET['id']);
-            $this->view('dashboard/purchase', array(
-                'products' => $products
-            ));
+        if(Auth::loggedIn()) {
+            $sale = new SaleModel();
+            if(isset($_GET['id'])) {
+                $products = $sale->products($_GET['id']);
+                $this->view('dashboard/purchase', array(
+                    'products' => $products
+                ));
+            }
+        } else {
+            $this->redirect('Auth', 'login');
         }
     }
 
     public function addProduct() {
-        $this->view('dashboard/addProduct');
+        if(Auth::loggedIn()) {
+            $this->view('dashboard/addProduct');
+        } else {
+            $this->redirect('Auth', 'login');
+        }
     }
 
     public function update() {
-        if(isset($_GET['id'])) {
-            $product = new ProductModel();
-            $products = $product->find($_GET['id']);
-            $this->view('dashboard/addProduct', array(
-                'product' => $products
-            ));
+        if(Auth::loggedIn()) {
+            if(isset($_GET['id'])) {
+                $product = new ProductModel();
+                $products = $product->find($_GET['id']);
+                $this->view('dashboard/addProduct', array(
+                    'product' => $products
+                ));
+            }
+        } else {
+            $this->redirect('Auth', 'login');
         }
     }
 } 
